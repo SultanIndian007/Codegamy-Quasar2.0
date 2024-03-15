ad5e3dbca76c9ce6f47c9882685e920ef383cebf
Kunal Agrawal <git@kunalag.simplelogin.com>
2024-03-15 16:03:33 +0530

    more fixes

--- models/UserInfo.js
+++ models/UserInfo.js
@@ -10,7 +10,11 @@ const UserInfoSchema = new Schema({
   phone: {type: String},
   admin: {type: Boolean, default: false},
   coursesEnrolled: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-  solved: [{ type: Schema.Types.ObjectId, ref: 'SolvedProblem' }]
+  solved: [{ type: Schema.Types.ObjectId, ref: 'SolvedProblem' }],
+  contestPart: [{ type: Schema.Types.ObjectId, ref: 'Contest' }],
+  peerVideo: [{ type: Schema.Types.ObjectId, ref: 'PeerVideo' }],
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'PeerViedoReview' }],
+
 }, {timestamps: true});
 
 export const UserInfo = models?.UserInfo || model('UserInfo', UserInfoSchema);
