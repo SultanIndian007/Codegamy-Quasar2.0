5a4ff87ac7249d18bbd8aa787a68bc6ef292b895
Deexith Madas <deexithmadas772@gmail.com>
2024-03-17 09:27:05 +0530

    professional interview

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,10 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import { authOptions } from "@/app/api/auth/[...nextauth]/route.js"
+import { getServerSession } from "next-auth/next"
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const session = await getServerSession(authOptions);
+  const userID = session?.user?._id;
   return (
     <div className='w-full max-w-6xl mx-auto flex justify-between items-center px-2 py-3'>
-      
+
       <div className='flex justify-between items-center gap-7'>
         <Link href='/'>
           <h2 className='font-bold text-xl'>
@@ -32,15 +37,26 @@ const Navbar = () => {
       </div>
 
       <div className='flex justify-between items-center gap-7'>
-        <Link href='/login'>
-          Login
-        </Link>
-        <Link href='/register'>
-          Register
-        </Link>
-        <Link href='/profile'>
-          Profile
-        </Link>
+        {
+          userID ? (
+            <>
+            <Link href='/profile'>
+              Profile
+            </Link>
+            <Link href='/login'>
+              Logout
+            </Link>
+            </>
+
+          ) : (
+            <><Link href='/login'>
+              Login
+            </Link>
+              <Link href='/register'>
+                Register
+              </Link></>
+          )
+        }
       </div>
 
     </div>
