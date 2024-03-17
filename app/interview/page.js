a32e6044421342a7952b1a6775465e866cf581fa
Ganesh Utla <ganeshutla888@gmail.com>
2024-03-17 07:51:20 +0530

    updated some ux

--- app/interview/page.js
+++ app/interview/page.js
@@ -6,12 +6,20 @@ import React, { useEffect, useState } from 'react'
 
 const page = () => {
 
-    const interviews = [];
     const [assignedInterviews, setAssignedInterviews] = useState(null);
-    const [assInterviewDetails, setAssInterviewDetails] = useState(null);
-    const [isAssInterviewModalOpen, setIsAssInterviewModalOpen] = useState(false);
+    const [interviews, setInterviews] = useState(null);
+    const [interviewDetails, setInterviewDetails] = useState(null);
+    const [isInterviewModalOpen, setIsInterviewModalOpen] = useState(false);
     const [feedback, setFeedback] = useState({ rating: 3, comment: "" });
     const [submitted, setSubmitted] = useState(false);
+    const [isAssigned, setIsAssigned] = useState(false);
+
+    const fetchInterviews = async () => {
+        const res = await fetch('/api/getMyVideos');
+        const data = await res.json();
+        setInterviews(data);
+        // console.log(data)
+    }
 
     const fetchAssignedInterviews = async () => {
         const res = await fetch('/api/getAssigned');
@@ -22,16 +30,17 @@ const page = () => {
     const handleSubmitReview = async () => {
         
         await axios.post('/api/submitReview', {
-            queueID: assInterviewDetails._id,
+            queueID: interviewDetails._id,
             rating: feedback.rating,
             comment: feedback.comment
         });
         setSubmitted(true);
-        setTimeout(() => {setSubmitted(false); setIsAssInterviewModalOpen(false);}, 2000);
+        setTimeout(() => {setSubmitted(false); setIsInterviewModalOpen(false);}, 2000);
     }
 
     useEffect(() => {
         fetchAssignedInterviews();
+        fetchInterviews();
     }, []);
 
   return (
@@ -55,9 +64,14 @@ const page = () => {
                 <h2 className='font-medium mb-3'>
                     Past Interviews
                 </h2>
-                <div className=''>
-                    {interviews.length > 0? (
-                        <div></div>
+                <div className='w-full flex gap-10 flex-wrap'>
+                    {interviews && interviews.length > 0? (
+                        interviews.map((interview, index) => (
+                            <div key={index} onClick={() => {setInterviewDetails(interview); setIsInterviewModalOpen(true); setIsAssigned(false);}}
+                            className='w-[300px] rounded-lg cursor-pointer shadow-lg bg-light-2 py-14 text-center hover:bg-light-3 transition-all'>
+                                {`My Interview - ${index+1}`}<br/>{`Submitted: ${interview.submissionTime}`}
+                            </div>
+                        ))
                     ) : (
                         <p className='text-gray-2'>No interviews found</p>
                     )}
@@ -71,7 +85,7 @@ const page = () => {
                 <div className='w-full flex gap-10 flex-wrap'>
                     {assignedInterviews && assignedInterviews.assigned && assignedInterviews.assigned.length > 0? (
                         assignedInterviews.assigned.map((interview, index) => (
-                            <div key={index} onClick={() => {setAssInterviewDetails(interview); setIsAssInterviewModalOpen(true);}}
+                            <div key={index} onClick={() => {setInterviewDetails(interview); setIsInterviewModalOpen(true); setIsAssigned(true);}}
                             className='w-[300px] rounded-lg cursor-pointer shadow-lg bg-light-2 py-14 text-center hover:bg-light-3 transition-all'>
                                 {`Assigned interview - ${index+1}`}<br/>{`Deadline: ${assignedInterviews.assignedTime[index]}`}
                             </div>
@@ -84,56 +98,105 @@ const page = () => {
         </div>
 
 
-        { isAssInterviewModalOpen && (
+        { isInterviewModalOpen && (
             <div className='fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center'>
                 <div className='bg-light-1 w-[600px] max-h-[92vh] px-10 py-12 shadow-lg mx-auto rounded-lg overflow-y-auto'>
                     <div className='w-full text-right cursor-pointer'>
-                        <p onClick={() => setIsAssInterviewModalOpen(false)}>X</p>
+                        <p onClick={() => setIsInterviewModalOpen(false)}>X</p>
                     </div>
-                    <div className='flex flex-col gap-5 items-center'>
-                        <p>Q: {assInterviewDetails.peerVideo.question.question}</p>
-                        <video
-                            src={`/assets/${assInterviewDetails.peerVideo.videoUrl}`}
-                            controls
-                            loop
-                            controlsList="nofullscreen nodownload noremoteplayback noplaybackrate foobar"
-                            className="w-[560px] h-auto"
-                        />
-                        <div>
-                            <p>Rating</p>
-                            <div className='flex items-center gap-1'>
-                                <p onClick={() => setFeedback({...feedback, rating: 1})} className={`${feedback.rating >= 1 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
-                                    1
-                                </p>
-                                <p onClick={() => setFeedback({...feedback, rating: 2})} className={`${feedback.rating >= 2 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
-                                    2
-                                </p>
-                                <p onClick={() => setFeedback({...feedback, rating: 3})} className={`${feedback.rating >= 3 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
-                                    3
-                                </p>
-                                <p onClick={() => setFeedback({...feedback, rating: 4})} className={`${feedback.rating >= 4 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
-                                    4
-                                </p>
-                                <p onClick={() => setFeedback({...feedback, rating: 5})} className={`${feedback.rating >= 5 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
-                                    5
-                                </p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <p>Comment</p>
-                            <textarea 
-                                className='rounded-lg'
-                                rows={5}
-                                value={feedback.comment}
-                                onChange={(e) => setFeedback({...feedback, comment: e.target.value})}
+                    {isAssigned? (
+                        
+                        <div className='flex flex-col gap-5 items-center'>
+                            <p>Q: {interviewDetails.peerVideo.question.question}</p>
+                            <video
+                                src={`/assets/${interviewDetails.peerVideo.videoUrl}`}
+                                controls
+                                loop
+                                controlsList="nofullscreen nodownload noremoteplayback noplaybackrate foobar"
+                                className="w-full h-auto"
                             />
-                        </div>
+                            <div className="w-full">
+                                <p>Rating</p>
+                                <div className='flex items-center gap-1'>
+                                    <p onClick={() => setFeedback({...feedback, rating: 1})} className={`${feedback.rating >= 1 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        1
+                                    </p>
+                                    <p onClick={() => setFeedback({...feedback, rating: 2})} className={`${feedback.rating >= 2 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        2
+                                    </p>
+                                    <p onClick={() => setFeedback({...feedback, rating: 3})} className={`${feedback.rating >= 3 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        3
+                                    </p>
+                                    <p onClick={() => setFeedback({...feedback, rating: 4})} className={`${feedback.rating >= 4 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        4
+                                    </p>
+                                    <p onClick={() => setFeedback({...feedback, rating: 5})} className={`${feedback.rating >= 5 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        5
+                                    </p>
+                                </div>
+                            </div>
 
-                        <button onClick={handleSubmitReview} className="text-light-1 bg-blue-500 py-2 px-3 rounded-lg">
-                            Submit Review
-                        </button>
-                    </div>
+                            <div className='w-full'>
+                                <p>Comment</p>
+                                <textarea 
+                                    className='w-full rounded-lg'
+                                    rows={5}
+                                    value={feedback.comment}
+                                    onChange={(e) => setFeedback({...feedback, comment: e.target.value})}
+                                />
+                            </div>
+
+                            <button onClick={handleSubmitReview} className="text-light-1 bg-blue-500 py-2 px-3 rounded-lg">
+                                Submit Review
+                            </button>
+                        </div>
+                    ) : (
+
+                        <div className='flex flex-col gap-5 items-center'>
+                            <p>Q: {interviewDetails.question.question}</p>
+                            <video
+                                src={`/assets/${interviewDetails.videoUrl}`}
+                                controls
+                                loop
+                                controlsList="nofullscreen nodownload noremoteplayback noplaybackrate foobar"
+                                className="w-full h-auto"
+                            />
+                            <div className="w-full">
+                                <p>Rating</p>
+                                {/* <div className='flex items-center gap-1'>
+                                    <p className={`${interviewDetails >= 1 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        1
+                                    </p>
+                                    <p className={`${feedback.rating >= 2 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        2
+                                    </p>
+                                    <p className={`${feedback.rating >= 3 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        3
+                                    </p>
+                                    <p className={`${feedback.rating >= 4 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        4
+                                    </p>
+                                    <p className={`${feedback.rating >= 5 && 'bg-yellow-500'} p-1 px-2 rounded-full`}>
+                                        5
+                                    </p>
+                                </div> */}
+                            </div>
+
+                            <div className='w-full'>
+                                <p>Comment</p>
+                                <textarea 
+                                    className='w-full rounded-lg'
+                                    rows={5}
+                                    value={feedback.comment}
+                                    onChange={(e) => setFeedback({...feedback, comment: e.target.value})}
+                                />
+                            </div>
+
+                            <button className="text-light-1 bg-blue-500 py-2 px-3 rounded-lg">
+                                Submit Review
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         )}
--- app/learn/CourseCard.js
+++ app/learn/CourseCard.js
@@ -4,17 +4,16 @@ import { Button, Card, CardBody, CardTitle, CardText, Progress } from 'reactstra
 import { useRouter } from 'next/navigation';
 
 
-const CourseCard = ({ name, title, desc, progress, button }) => {
+const CourseCard = ({ name, title, desc, progress, button, image }) => {
     const router = useRouter();
 
     return (
         <Card style={{backgroundColor: '#f8f9fa',
             borderRadius: '10px',
             width: '300px',
-            height: '250px',
             boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
             borderColor:"#f8f9fa",
-            margin: '20px'
+            margin: '15px 20px'
         }}
         >
             <CardBody className='flex flex-col p-4 justify-between gap-4 h-full'>
@@ -24,7 +23,12 @@ const CourseCard = ({ name, title, desc, progress, button }) => {
                 <CardText
                     className='text-sm flex-grow'
                 >{desc}</CardText>
-                <Progress value={50}  style={{height: '10px', borderRadius: '5px'}}/>
+                {/* <Progress value={50}  style={{height: '10px', borderRadius: '5px'}}/> */}
+                <img
+                    src={image}
+                    alt={title}
+                    className='w-24 h-24 mb-2 object-contain rounded-lg mx-auto'
+                />
                 <Button
                     className='w-full hover:bg-blue-800 hover:border-blue-900  text-light-1 p-2 rounded-lg bg-blue-600 border-blue-700 text-sm font-thin transition-all'
                     onClick={() => router.push(`/courses/${name}`)}
